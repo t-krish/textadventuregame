fb472a9ecba12564553dd6a6e64d721b6da8172d
krish402tated <95111595+t-krish@users.noreply.github.com>
2021-11-28 20:54:53 +0000

    Put new lines. Made the inventory. Made another option.

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ op1 = open('Option1.txt')
 op1Line = op1.readlines()
 op2 = open('Option2.txt')
 op2Line = op2.readlines()
+inv = []
 if gameStart == 1:
     print(sLine[0])
     option = input(oLine[0])
@@ -32,8 +33,39 @@ if gameStart == 1:
       print(op1Line[1])
       print(op2Line[1])
     elif option == 2:
-      print(op2Line[2])
+      print(op2Line[1])
     else:
       print('invalid')
+    print(sLine[3])
+    note = 'It said "7/3/01". Those numbers seem familiar, but you cannot remember. It also had an arrow, pointing to your left.'
+    print('\n')
+    print('You put the note in your inventory. Type inv at an option to print your inventory.')
+    inv.append('note: ' + note)
+    print('\n')
+    oi = input(oLine[2])
+    try:
+      oi = int(oi)
+    except:
+      oi == 'inv'
+    if oi == 1:
+      print(op1Line[2])
+      print('\n')
+      print(op2Line[2])
+    elif oi == 'inv':
+      print('\n')
+      print(inv)
+      oi = input(oLine[2])
+      print('\n')
+      oi = int(oi)
+      if oi == 1:
+        print(op1Line[2])
+        print('\n')
+        print(op2Line[2])
+      elif oi == 2:
+        print(op2Line[2])
+      else:
+        print('invalid')
+    elif oi == 2:
+      print(op2Line[2])
     s.close()
     o.close()
